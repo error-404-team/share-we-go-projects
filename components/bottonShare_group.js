bbc0b675c5813ad0a6ca8c8acad5d85d4ae8771f
Eakka_Com <benz-smart-007@hotmail.com>
2019-09-16 15:42:18 +0700

    update bottonShare

--- components/bottonShare_group.js
+++ components/bottonShare_group.js
@@ -2,12 +2,26 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
+import Fab from '@material-ui/core/Fab';
+
+
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
+
+
+
 const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
   },
   input: {
     display: 'none',
+  },
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
   },
 }));
 
@@ -23,6 +37,20 @@ export default function ContainedButtons() {
         เเชท
       </Button>
      
+      <div>
+      <Fab color="primary" aria-label="add" className={classes.fab}>
+          <LocalTaxiIcon />
+        </Fab>
+      </div>
+
+      <div>
+      <Fab color="primary" aria-label="add" className={classes.fab}>
+          <QuestionAnswerIcon />
+        </Fab> 
+      </div>
+
+      
     </div>
   );
 }
+
