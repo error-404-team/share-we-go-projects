971d77d0505eccfa047261a28f063f61258a2131
n-psk <my_nutthapat-26_@hotmail.com>
2019-10-01 06:31:12 +0700

    update msg

--- components/Messend/Appout.js
+++ components/Messend/Appout.js
@@ -2,44 +2,136 @@ import React from 'react'
 import InputData from './Inputdata'
 import ListData from './Listdata'
 import firebase from 'firebase'
+import { writeMessenger } from '../../firebase-database/write-data';
 import config from '../../lib/firebase'
 // firebase.initializeApp(Config)
 
 class Appout extends React.Component {
-  
-  state = { listMsg: [''] }
+
 
   constructor(props) {
     super(props)
-    var that  = this;
-    firebase.database().ref('message').on('value', function(snapshot) {
-      if(snapshot.val() != null) { 
-         that.setState({
-          listMsg:snapshot.val()
-         })
-       }
-    });
+    this.state = {
+      listMsg: [''],
+      uid: ''
+    }
+
+  }
+
+  componentDidMount() {
+    const me = this;
+    firebase.auth().onAuthStateChanged((user) => {
+      firebase.database().ref(`/group_share_user/keys`).once('value').then(function (snapshot) {
+        var keys = (snapshot.val());
+        if (keys !== null) {
+          Object.keys(keys).map((key) => {
+            if (key !== user.uid) {
+              firebase.database().ref(`/group_share_user/${key}/join/keys`).on('value', function (snapshot) {
+                let joinKeys = (snapshot.val());
+                if (joinKeys !== null) {
+                  Object.keys(joinKeys).map((key) => {
+                    if (key === user.uid) {
+                      firebase.database().ref(`/group_share_user/${key}/messengers`).on('value', function (snapshot) {
+                        let dataMsg = (snapshot.val());
+                        me.setState({
+                          listMsg: dataMsg,
+                          uid: user.uid
+                        })
+                      })
+                    }
+                  })
+                }
+              })
+
+            } else {
+              firebase.database().ref(`/group_share_user/${user.uid}`).once('value').then(function (snapshot) {
+                let stories = (snapshot.val());
+                if (stories.share === true) {
+                  firebase.database().ref(`/group_share_user/${key}/messengers`).on('value', function (snapshot) {
+                    let dataMsg = (snapshot.val());
+                    me.setState({
+                      listMsg: dataMsg,
+                      uid: user.uid
+                    })
+                  })
+                }
+              })
+            }
+          })
+        }
+      })
+
+    })
   }
 
   onClickButtonHandlerData = (msg) => {
-    const listMsgData = this.state.listMsg.concat({
-      key: Math.random().toString().replace('.',''),
-      message: msg
-     })
-    firebase.database().ref('message/').set(listMsgData);
+    
+    firebase.auth().onAuthStateChanged((user) => {
+      firebase.database().ref(`/group_share_user/keys`).once('value').then(function (snapshot) {
+        var keys = (snapshot.val());
+        if (keys !== null) {
+          Object.keys(keys).map((key) => {
+            if (key !== user.uid) {
+              firebase.database().ref(`/group_share_user/${key}/join/keys`).on('value', function (snapshot) {
+                let joinKeys = (snapshot.val());
+                if (joinKeys !== null) {
+                  Object.keys(joinKeys).map((key) => {
+                    if (key === user.uid) {
+                      firebase.database().ref(`/profile/${user.uid}`).once('value').then(function (snapshot) {
+                        let dataProfileUser = (snapshot.val());
+                        let data = {
+                          displayName: dataProfileUser.displayName,
+                          photoURL: dataProfileUser.photoURL,
+                          msg: msg,
+                          uid: user.uid,
+                          mid: Math.random().toString(36).substr(2, 9),
+                          header: false
+                        }
+                        writeMessenger(user.uid, data)
+                      })
+                    }
+                  })
+                }
+              });
+            } else {
+              firebase.database().ref(`/group_share_user/${user.uid}`).once('value').then(function (snapshot) {
+                let stories = (snapshot.val());
+                console.log('false',false);
+                if (stories.share !== false) {
+                  firebase.database().ref(`/profile/${user.uid}`).once('value').then(function (snapshot) {
+                    let dataProfileUser = (snapshot.val());
+                    let data = {
+                      displayName: dataProfileUser.displayName,
+                      photoURL: dataProfileUser.photoURL,
+                      msg: msg,
+                      uid: user.uid,
+                      mid: Math.random().toString(36).substr(2, 9),
+                      header: true
+                    }
+                    writeMessenger(user.uid, data)
+                  })
+                }
+              })
+            }
+          })
+        }
+      })
+
+    })
   }
 
 
   render() {
-    const listMsg = this.state.listMsg.map( msg => {
-      return <p>{msg.message}</p>
-    })
+    // const listMsg = this.state.listMsg.map(msg => {
+    //   return <p>{msg.message}</p>
+    // })
     return (
       <div>
         <ListData
-            listMsg = {this.state.listMsg} />
-        <InputData 
-            onClickButtonHandler = {this.onClickButtonHandlerData}
+          listMsg={this.state.listMsg}
+          uid={this.state.uid} />
+        <InputData
+          onClickButtonHandler={this.onClickButtonHandlerData.bind(this)}
         />
       </div>
     );
--- components/Messend/Listdata.js
+++ components/Messend/Listdata.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
 // import TEST from '../Img/TEST.jpg'
 // import Img from '../Messend/Photo/Img.jpeg'
 
@@ -11,7 +13,7 @@ const Styeles = {
         // float:'right'
     },
     InpuChatButton: {
-       
+
         backgroundColor: 'white',
         // borderTop: '0px solid #ddd',
         borderTopRightRadius: '20px',
@@ -29,67 +31,92 @@ const Styeles = {
 
 }
 
-const ListData = (props) => {
-    const { classes } = props
-    return (
-        <div>
-               
-        <div className={classes.InpuChatButton}>
-            <button
-                style={{
-                    border: '0px solid #ffffff00',
-                    backgroundColor: '#3f51b5',
-                    position: 'fixed',
-                    bottom: '480px',
-                    height: '77px',
-                    width: '77%',
-                    right: '15%',
-                    // borderTopLeftRadius: '18px',
-                    // borderTopRightRadius: '18px',
-                }}>
-           
-                   
-                {/* <InsertEmoticonOutlinedIcon>
+class ListData extends React.Component {
+
+    render() {
+        const { classes, listMsg, uid } = this.props
+
+        // Object.keys(listMsg).map((key,index)=> console.log(listMsg[key].msg))
+        return (
+            <div>
+                <div className={classes.InpuChatButton}>
+                    <button
+                        style={{
+                            border: '0px solid #ffffff00',
+                            backgroundColor: '#3f51b5',
+                            position: 'fixed',
+                            bottom: '480px',
+                            height: '77px',
+                            width: '77%',
+                            right: '15%',
+                            // borderTopLeftRadius: '18px',
+                            // borderTopRightRadius: '18px',
+                        }}>
+                        {/* <InsertEmoticonOutlinedIcon>
                             </InsertEmoticonOutlinedIcon> */}
-                <span>
-                    
-                    สมาชิก
-                            </span>
-            </button>
-            <div style={{
-                padding: 'inherit',
-                padding: '10px 15px',
+                        <span>สมาชิก</span>
+                    </button>
+                    <div style={{
+                        padding: '10px 15px',
+                        marginTop: '18%'
 
-            }}>
-                {
-                    props.listMsg.map(msg => {
-                        return <p
-                            style={{
-                                // backgroundColor: '#1C3657',
-                                bottom: '14%',
-                                padding: 'inherit',
-                                padding: '10px 15px',
-                                // position:'absolute',
-                                // textOverflow:'clip',
-                                // color: 'white',
-                                clear: 'both',
-                                borderRadius: '5px',
-                                // width:'100%'
-                                float: 'right',
+                    }}>
+                        {listMsg !== null
+                            ? <React.Fragment>
+                                {Object.keys(listMsg).map(key => (
+                                    <React.Fragment key={key}>
+                                        {listMsg[key].uid === uid
+                                            ? <React.Fragment key={key}>
+                                                <p style={{
+                                                    bottom: '14%',
+                                                    padding: 'inherit',
+                                                    padding: '10px 15px',
+                                                    clear: 'both',
+                                                    borderRadius: '5px',
+                                                    float: 'right',
+                                                    color: 'blue'
+                                                }}
+                                                    key={key}>{listMsg[key].msg}
+                                                </p>
+                                            </React.Fragment>
+                                            : <React.Fragment key={key}>
+                                                <Chip
+                                                    key={key}
+                                                    avatar={<Avatar alt="Natacha" src={listMsg[key].photoURL} />}
+                                                    label={(
+                                                        <div>
+                                                            <h3>{listMsg[key].displayName}</h3>
+                                                            <p>{listMsg[key].msg}</p>
+                                                        </div>
+                                                    )}
+                                                />
+                                            </React.Fragment>
+                                        }
+                                    </React.Fragment>
+                                ))
+                                }
+                            </React.Fragment>
+                            : <React.Fragment>
+                                <center>
+                                    <p style={{
+                                        bottom: '14%',
+                                        padding: 'inherit',
+                                        padding: '10px 15px',
+                                        clear: 'both',
+                                        borderRadius: '5px',
+                                        float: 'left',
+                                        color: 'black'
+                                    }}>พิมพ์ข้อความลงไปเพื่สนทนาตอบโต้</p>
+                                </center>
+                            </React.Fragment>
+                        }
+                    </div>
 
-                            }}
-                            key={msg.key}>{msg.message}
-                        </p>
-
-                    })
-                }
+                </div>
             </div>
 
-        </div>
-        </div>
-
-    )
-
+        )
+    }
 }
 
 // export default ListData;
