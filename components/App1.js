b56a2a46cca9c6796927716229c0ab04997faae8
anonhtml <35714813+anonhtml@users.noreply.github.com>
2019-10-01 12:30:10 +0700

    up

--- components/App1.js
+++ components/App1.js
@@ -1,8 +1,6 @@
 import React from 'react';
 // import { bool } from 'prop-types';
-import m1 from '../../my-app/src/image/m1.png'
-import m21 from '../../my-app/src/image/m21.png'
-import m31 from '../../my-app/src/image/m31.png'
+
 
 class App1 extends React.Component{
   render() {
@@ -14,15 +12,15 @@ class App1 extends React.Component{
           </body>
           
             <body>
-            <h4 align='middle'align='right'> <img src={m1} border="0" width="200" height="25"  vspace="0" hspace="40"></img>< a href="tel:0865425625">โทร:086-542-5625</a> </h4>
+            <h4>< a href="tel:0865425625">โทร:086-542-5625</a> </h4>
              <h5 align='right'>(ค่าบริการ 20 บาท)</h5>
              <hr size="1" width="400"></hr> 
 
-             <h4 align='middle'align='right'> <img src={m21} border="0" width="200" height="25"  vspace="0" hspace="40"></img>< a href="tel:1681">โทร:1681</a></h4>
+             <h4 >< a href="tel:1681">โทร:1681</a></h4>
              <h5 align='right'>(ค่าบริการ 20 บาท)</h5>
              <hr size="1" width="400"></hr>
              
-             <h4 align='middle' align='right'><img src={m31} border="0" width="200" height="25" vspace="0" hspace="55"></img>< a href="tel:020189799">โทร:0-2018-9799</a></h4>
+             <h4 >< a href="tel:020189799">โทร:0-2018-9799</a></h4>
              <h5 align='right'>(ค่าบริการ 40 บาท)</h5>
 
              <body bgcolor= "dddddd"> 
